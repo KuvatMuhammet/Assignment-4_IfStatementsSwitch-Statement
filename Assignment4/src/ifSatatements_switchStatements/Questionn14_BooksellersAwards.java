543f0e48b2eb5077bf0836f66e45f5bb874324e0
KuvatMuhammet <71410580+KuvatMuhammet@users.noreply.github.com>
2020-10-16 20:39:28 +0200

    Update Questionn14_BooksellersAwards.java

--- Assignment4/src/ifSatatements_switchStatements/Questionn14_BooksellersAwards.java
+++ Assignment4/src/ifSatatements_switchStatements/Questionn14_BooksellersAwards.java
@@ -5,6 +5,17 @@ import java.util.Scanner;
 public class Questionn14_BooksellersAwards {
 
 	public static void main(String[] args) {
+		/*
+		ABC Booksellers has a book club that awards points to its customers based on the number of books purchased each month. 
+		The points are awarded as follows:
+		If a customer purchases 0 books, he or she earns 0 points.
+		If a customer purchases 1 book, he or she earns 5 points.
+		If a customer purchases 2 books, he or she earns 15 points.
+		If a customer purchases 3 books, he or she earns 30 points.
+		If a customer purchases 4 books or more books, he or she earns 60 points.
+		Write a program that lets the user declare the number of books that he or she has purchased 
+		this month and then display the number of points awarded.
+		*/
 		
 		Scanner scan = new Scanner(System.in);
 		
