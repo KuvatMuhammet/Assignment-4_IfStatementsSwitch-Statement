213d1b73f718957677c82654a0d3b1b0fa33beee
KuvatMuhammet <71410580+KuvatMuhammet@users.noreply.github.com>
2020-10-16 20:37:16 +0200

    Update Questionn12_InternetCharges.java

--- Assignment4/src/ifSatatements_switchStatements/Questionn12_InternetCharges.java
+++ Assignment4/src/ifSatatements_switchStatements/Questionn12_InternetCharges.java
@@ -3,6 +3,14 @@ package ifSatatements_switchStatements;
 import java.util.Scanner;
 
 public class Questionn12_InternetCharges {
+		/*
+		An internet service provider has three different subscription packages for its customers:
+		Package A: For $9.95 per month 10 hours of access are provided. Additional hours are $2.00 per hour
+		Package B: For $13.95 per month 20 hours of access are provided. Additional hours are $1.00 per hour
+		Package C: For $19.95 per month unlimited access is provided
+		Write a program that calculates a customer's monthly bill. It should let the user declare the letter of the package 
+		the customer has purchased (A, B, or C) and the number of hours that were used. It should then display the total charges.
+		*/
 
 	public static void main(String[] args) {
 		
