36259b6c1ca56b360ac39c0014f92d0983133550
KuvatMuhammet <71410580+KuvatMuhammet@users.noreply.github.com>
2020-10-16 20:38:15 +0200

    Update Questionn13_BankCharges.java

--- Assignment4/src/ifSatatements_switchStatements/Questionn13_BankCharges.java
+++ Assignment4/src/ifSatatements_switchStatements/Questionn13_BankCharges.java
@@ -5,6 +5,15 @@ import java.util.Scanner;
 public class Questionn13_BankCharges {
 
 	public static void main(String[] args) {
+		/*
+		A bank charges a base fee of $10 per month, plus the following check fees for a commercial checking account:
+                $.10 each for less than 20 checks
+                $.08 each for 20-39 checks  
+                $.06 each for 40-59 checks
+                $.04 each for 60 or more checks
+		Write a program that lets the user declare the number of checks written for the month. 
+		The program should then calculate and display the bank's service fees for the month. 
+		*/
 		
 		Scanner scan = new Scanner(System.in);
 				
