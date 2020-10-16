049aed5a377e2a744d16457594ac1b308a896a53
KuvatMuhammet <71410580+KuvatMuhammet@users.noreply.github.com>
2020-10-16 20:22:32 +0200

    Update Question2_TaxCalculator.java

--- Assignment4/src/ifSatatements_switchStatements/Question2_TaxCalculator.java
+++ Assignment4/src/ifSatatements_switchStatements/Question2_TaxCalculator.java
@@ -3,6 +3,26 @@ package ifSatatements_switchStatements;
 import java.util.Scanner;
 
 public class Question2_TaxCalculator {
+	/*
+		Tax Calculator:
+		    - Let user enter his income
+		    - Calculate the tax amount according to below table
+		    - Print the tax amount
+		Income		     Tax Ratio
+		income ≤150,000,000	%25
+		income ≤ 300,000,000	%30
+		income ≤ 600,000,000	%35
+		income ≤ 1,200,000,000	%40
+		income > 1,200,000,000	%50
+		Sample Output-1
+		Your Income : 1500
+		The tax amount you have to pay : 375.0
+		Sample Output-2
+		Your Income : 1200000
+		The tax amount you have to pay : 300000.0
+		Note: IF Your Income is 200,000,000, your tax will be calculated this way: 
+		%25 for 150,000,000 and %30 for the rest of the amount which is 50,000,000
+	*/
 
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
