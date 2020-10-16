fe98cf42dfe1cbe97191a0712f6eb35346dfb366
KuvatMuhammet <71410580+KuvatMuhammet@users.noreply.github.com>
2020-10-16 20:36:09 +0200

    Update Questionn11_SpeedOfSound.java

--- Assignment4/src/ifSatatements_switchStatements/Questionn11_SpeedOfSound.java
+++ Assignment4/src/ifSatatements_switchStatements/Questionn11_SpeedOfSound.java
@@ -3,6 +3,21 @@ package ifSatatements_switchStatements;
 import java.util.Scanner;
 
 public class Questionn11_SpeedOfSound {
+		/*
+		The following table shows the approximate speed of sound in air, water, and steel:
+		Medium	Speed
+		Air	1100 feet per second
+		Water	4900 feet per second
+		Steel	16400 feet per second
+		Write a program that let the user declare a variable to enter "air", "water",  or "steel", and distance that 
+		a sound wave will travel in the medium. The program should then display the amount of time it will take. 
+		You can calculate the amount of time it takes sound to travel in the air with the following formula:
+                  Time=Distance/1100
+		You can calculate the amount of time it takes sound to travel in water with the following formula:
+                  Time=Distance/4900
+		You can calculate the amount of time it takes sound to travel in steel with the following formula:
+                  Time=Distance/16400
+		*/
 
 	public static void main(String[] args) {
 		
