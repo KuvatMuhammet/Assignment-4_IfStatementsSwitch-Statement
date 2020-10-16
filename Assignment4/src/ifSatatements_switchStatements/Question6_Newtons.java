cac591873dd2ce24368f1e56de804041b4e5ffce
KuvatMuhammet <71410580+KuvatMuhammet@users.noreply.github.com>
2020-10-16 20:28:32 +0200

    Update Question6_Newtons.java

--- Assignment4/src/ifSatatements_switchStatements/Question6_Newtons.java
+++ Assignment4/src/ifSatatements_switchStatements/Question6_Newtons.java
@@ -5,6 +5,14 @@ import java.util.Scanner;
 public class Question6_Newtons {
 
 	public static void main(String[] args) {
+		/*
+		Scientists measure an object's mass in kilograms and its weight in Newton. If you know the amount of mass that an object has, 
+		you can calculate its weight, in Newtons, with the following formula:
+        	     Weight = Mass * 9.8
+		Write a program that asks the user to enter an object's mass, and then calculate its weight. If the object weighs more than 1000 Newtons, 
+		display a message indicating that it is too heavy. If the object weighs less than 10 Newtons, 
+		display a message indicating that the object is too light.
+		*/
 
 		Scanner scan = new Scanner(System.in);
 		
