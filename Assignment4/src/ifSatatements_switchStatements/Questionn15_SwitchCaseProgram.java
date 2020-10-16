5547df8d39720bce503bbb16e15bba6e40a78a50
KuvatMuhammet <71410580+KuvatMuhammet@users.noreply.github.com>
2020-10-16 20:40:34 +0200

    Update Questionn15_SwitchCaseProgram.java

--- Assignment4/src/ifSatatements_switchStatements/Questionn15_SwitchCaseProgram.java
+++ Assignment4/src/ifSatatements_switchStatements/Questionn15_SwitchCaseProgram.java
@@ -5,6 +5,12 @@ import java.util.Scanner;
 public class Questionn15_SwitchCaseProgram {
 
 	public static void main(String[] args) {
+		/*
+			Write a switch-case program:
+			1- Declare a number
+			2- IF the number is 0,1 or 2 print "Low Number. IF the number is 3,4,5 print "Medium Number". 
+			IF the number is is not between 0-5 then print "Other Number"
+		*/
 		Scanner scan = new Scanner(System.in);
 		
 		System.out.println("Enter the number:");
