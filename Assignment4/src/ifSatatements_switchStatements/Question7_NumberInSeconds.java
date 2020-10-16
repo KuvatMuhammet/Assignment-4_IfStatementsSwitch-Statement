e2cc0ae52f5ff0d67e8104a27ede3d30d0ab3d6a
KuvatMuhammet <71410580+KuvatMuhammet@users.noreply.github.com>
2020-10-16 20:29:44 +0200

    Update Question7_NumberInSeconds.java

--- Assignment4/src/ifSatatements_switchStatements/Question7_NumberInSeconds.java
+++ Assignment4/src/ifSatatements_switchStatements/Question7_NumberInSeconds.java
@@ -5,6 +5,16 @@ import java.util.Scanner;
 public class Question7_NumberInSeconds {
 
 	public static void main(String[] args) {
+		/*
+		Write a program that lets the user declare a number in seconds.
+
+		There are 60 seconds in a minute. If the number of seconds is greater than or equal to 60, 
+		the program should display the number of minutes in that many seconds.
+		There are 3600 seconds in an hour. If the number of seconds is greater than or equal to 3600, 
+		the program should display the number of hours in that many seconds.
+		There are 86400 seconds in a day. If the number of seconds is greater than or equal to 86400, 
+		the program should display the number of days in that many seconds.
+		*/
 		
 		Scanner scan = new Scanner(System.in);
 		
