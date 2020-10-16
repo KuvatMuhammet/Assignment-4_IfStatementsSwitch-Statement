204fee77f49bb3e31f5b1b6800dbffebf4f08ffa
KuvatMuhammet <71410580+KuvatMuhammet@users.noreply.github.com>
2020-10-16 20:23:37 +0200

    Update Question3_LeapYear.java

--- Assignment4/src/ifSatatements_switchStatements/Question3_LeapYear.java
+++ Assignment4/src/ifSatatements_switchStatements/Question3_LeapYear.java
@@ -3,6 +3,15 @@ package ifSatatements_switchStatements;
 import java.util.Scanner;
 
 public class Question3_LeapYear {
+	/*
+		Any year is input by the user. Write a program to determine whether the year is a leap year or not.
+		Leap Years are any year that can be evenly divided by 4.  A year that is evenly divisible by 100 is a leap year only if it is also evenly divisible by 400.
+
+		Example : 
+		1992      Leap Year
+		2000      Leap Year
+		1900      NOT a Leap Year
+	*/
 
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
